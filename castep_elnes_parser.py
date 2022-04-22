fcfc388fbdbfcc301afb32c53ee3431d9e09f2a7
kiyou <46545721+kiyou@users.noreply.github.com>
2022-04-22 09:12:56 +0000

    [mod] add version info in main script

--- castep_elnes_parser.py
+++ castep_elnes_parser.py
@@ -26,6 +26,11 @@ import re
 import struct
 import numpy as np
 
+# Semantic Versioning
+# https://semver.org/
+# (MAJOR, MINOR, PATCH)
+VERSION = (1, 0, 1)
+__version__ = '.'.join(map(str, VERSION))
 
 def split_castep(filename):
     """
